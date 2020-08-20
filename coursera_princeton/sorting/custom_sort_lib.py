03a2aee4c12080cccca7ed63c79b6a8c84bc795c
KauGon <31174653+kaugon@users.noreply.github.com>
2020-08-20 11:43:36 -0700

    Update custom_sort_lib.py
    fixed redundant compare in selection sort

--- coursera_princeton/sorting/custom_sort_lib.py
+++ coursera_princeton/sorting/custom_sort_lib.py
@@ -24,7 +24,7 @@ def selection_sort(data):
     n = len(data)
     for i in range(n):
         min_index = i
-        for j in range(i, n):
+        for j in range(i+1, n):
             itr += 1
             if data[j] < data[min_index]:
                 min_index = j
